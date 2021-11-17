57a4ae1f0c99125f5006735db717255a6b2d9c5c
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-11-17 18:55:42 -0500

    Added directions for the second part of using file system module

--- ReadMe.md
+++ ReadMe.md
@@ -2,6 +2,8 @@
 
 ### Create a program with the file system module for (Creating,Reading,Deleting,Renaming) Files
 
+### Create a program with the file system module for (Creating and Deleting ) Folders
+
 ### From the file system module, understand and utilize:
 
 - writeFile()
@@ -9,8 +11,13 @@
 - rename()
 - appendFile()
 - unlink()
+- mkdir()
+- rmdir()
+- readdir()
 
 ### Create a separate file with entered argument using the writeFile() method
+
+### Create a folder and delete a folder
 
 ### Understand and describe what a buffer stream is
 
--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,8 @@
 // 1. Declare a const called fs and initialize it with the 'fs' import to import the file system module
 const fs = require('fs');
-// 2. Create and read a file using the writeFile method from file system module
-// fs.writeFile('myFile.txt', 'Hi everyone, this is my file!', (err: string) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log('file has been created');
-// 		fs.readFile('myFile.txt', 'utf-8', (err: string, data: string) => {
-// 			if (err) {
-// 				console.log(err);
-// 			} else {
-// 				console.log(data);
-// 			}
-// 		});
-// 	}
-// });
-fs.writeFile('test.ts', 'const greeting = "hello world"', (err: string) => {
-	err ? console.log(err) : console.log('File created successfully');
-});
-// 3. Rename a file using the readFile method from file system module
-// fs.rename('myFile.txt', 'myIntro.txt', (err: string) => {
-// 	if (err) {
-// 		console.log(err);
-// 	} else {
-// 		console.log('File has been renamed successfully.');
-// 	}
-// });
 
-// 4. Append content to the file using the appendFile method from file system module
-// fs.appendFile(
-// 	'myIntro.txt',
-// 	' I will be a huge success to the world by the time I am 40!',
-// 	(err: string) => {
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			console.log('Content successfully appended to file.');
-// 		}
-// 	}
-// );
-// 5. Delete file using the unlink method from file system module
-// fs.unlink('myIntro.txt', (err: string) => {
-// 	err ? console.log(err) : console.log('File successfully deleted!');
-// });
+// 2. Create a scenario where you create a folder and then a file within that folder
+
+// 3. Create a scenario where you delete a file within the folder and then the folder itself
+
+// 4. Create a scenario where you can delete multiple files within a folder and then delete the folder itself
