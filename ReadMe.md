7530596610af9564c1b35194d85e8f1627f38369
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-11-19 15:17:38 -0500

    Added directions as to how to server static files through http

--- ReadMe.md
+++ ReadMe.md
@@ -2,11 +2,6 @@
 
 ### From the http module, understand and utilize:
 
-- createServer() - makes your device an HTTP web server
-- server.listen() - Starts the HTTP server listening for connections.
+- response.writeHead() - sends a response header to the request with status code and response headers
 
-### Explain what the http module does
-
-Basically the http module turns your pc into a web server for browser to communicate with via URL
-Port numbers range from 0 to 65535, but only port numbers 0 to 1023 are reserved for privileged services and designated as well-known ports.
-[!Port Numbers](https://www.webopedia.com/reference/well-known-tcp-port-numbers/)
+### Serve the following static files using the http module and file system module
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
-// 1. Import the http module
-// 2. Declare a const called server and initialize it with createServer method which takes a callback function with request and response parameters.
-//    a. If the request has a path url of '/' then respond with 'Hello world from nodejs'.
-//    b. otherwise respond with 'Using some other domain
-// 3. Start listening for requests by listening for port number of your choice
+import { IncomingMessage, ServerResponse } from 'http';
+
+const http = require('http');
+const fs = require('fs');
+
+http
+	.createServer((req: IncomingMessage, res: ServerResponse) => {
+		const readStream = fs.createReadStream('./static/example.jpg');
+		res.writeHead(200, { 'Content-Type': 'image/jpg' });
+		readStream.pipe(res);
+	})
+	.listen(3000);
