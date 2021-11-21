6008327a70068da358b1aa8cdfaebbca11d4e1ac
Jose De La Cruz <jose.delcruz0490@icloud.com>
2021-11-20 22:14:07 -0500

    Added further directions to the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,5 @@
 // 1. Import http and fs modules
-// 2. Create a server where you can server either, html, images or json files statically
+// 2. Create a server where you can serve either, html, images or json files statically
+// a. Declare a const readStream and initialize it with the file system module's createReadStream method
+// b. Use the response writeHead method to enter the HTTP code and Content Type
+// c. Use the stream.pipe method to feed the response which is a writeable stream
